9d8369be7b04559a5fab85ce5154b3c94ba45865
mr_lipovsky <pe.lipovsky@gmail.com>
2022-06-01 18:42:35 +0200

    ap

--- api_app/views.py
+++ api_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .services import get_droplets
 
 class GetApi(TemplateView):
     template_name = 'index.html'
     def get_context_data(self, *args, **kwargs):
-        pass
+        context = {
+            'droplets' : get_droplets(),
+        }
+        return context
 
 # Create your views here.
