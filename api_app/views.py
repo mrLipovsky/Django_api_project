adbd2a1fd83e25d5a23aa0e296ab08f303aec732
mr_lipovsky <pe.lipovsky@gmail.com>
2022-06-13 22:21:20 +0200

    new nutrition api
    new nutrition api

--- api_app/views.py
+++ api_app/views.py
@@ -1,29 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+
 import requests
-import json
-
-def home(request):
-    url = ('https://newsapi.org/v2/everything?'
-       'q=Apple&'
-       'from=2022-06-01&'
-       'sortBy=popularity&'
-       'apiKey=a6aba2d161234810abdf29e32db175dd')
-
-    response = requests.get(url)
-    text = json.dumps(home, sort_keys=True, indent=4)
-    print(text)
-
-    print(response.json())
 
 
-    return render(request, 'api_app/index.html', {
-        "author": "Al Root",
-        'name': "Barron's",
-    })
-    
-    
-
-    # create a formatted string of the Python JSON object
-    
 # Create your views here.
+def index(request):
+    query = '1lb brisket and fries'
+    
+    my_headers={'X-Api-Key': 'v/q8BJaQOy88PdffmedUzw==SSfbsGeRYFTzZY2w'}
+    api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
+    
+    response = requests.get(api_url, headers=my_headers)
+    # if response.status_code == requests.codes.ok:
+    #     print(response.text)
+    return render(request,'index.html',{'response':response})
+    # else:
+    #     print("Error:", response.status_code, response.text)
+
+    
+ 
